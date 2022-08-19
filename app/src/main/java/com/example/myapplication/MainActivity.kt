f32f610220eb91e0d7f8c5e985200714cdc32db7
catmiih <emilyleme.dev@gmail.com>
2022-08-19 10:52:53 -0300

    Funcionando

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,89 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
 
-        val btnCadastro : TextView = findViewById(R.id.register)
+        val edtLogin: EditText = findViewById(R.id.user)
+        val edtPass: EditText = findViewById(R.id.pass)
+
+        val inputLogin = edtLogin.text
+        val inputPass = edtPass.text
+
+        val btnCadastro : Button = findViewById(R.id.register)
         btnCadastro.setOnClickListener{
             val intent = Intent(this, Register::class.java)
+            startActivity(intent);
+        }
+
+        val btnAccess : Button = findViewById(R.id.access)
+        btnAccess.setOnClickListener{
+            if (inputLogin.isEmpty() || inputPass.isEmpty()) {
+                Toast.makeText(this,"O campo login ou senha está vazio",Toast.LENGTH_SHORT).show()
+            }  else
+                login(inputLogin.toString(), inputPass.toString())
         }
     }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
+        }
+    }
+
+    private fun login(email:String, password:String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    Toast.makeText(baseContext, "Bem vindo!",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun reload() {
+
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
 }
--- app/src/main/java/com/example/myapplication/Register.kt
+++ app/src/main/java/com/example/myapplication/Register.kt
@@ -2,24 +2,49 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        auth = Firebase.auth
+        val user = Firebase.auth.currentUser
+
         val edtLogin: EditText = findViewById(R.id.user)
         val edtPass: EditText = findViewById(R.id.pass)
 
-        val inputLogin = edtLogin.text.toString()
-        val inputPass = edtPass.text.toString()
+        val inputLogin = edtLogin.text
+        val inputPass = edtPass.text
 
-        if (inputLogin.isEmpty() || inputPass.isEmpty()) {
-            Toast.makeText(this,"O campo login ou senha está vazio",LENGTH_SHORT).show()
-            return
+        val btnRegister : Button = findViewById(R.id.register)
+        btnRegister.setOnClickListener{
+            if (inputLogin.isEmpty() || inputPass.isEmpty()) {
+                Toast.makeText(this,"O campo login ou senha está vazio",LENGTH_SHORT).show()
+            }
+                createAccount(inputLogin.toString(), inputPass.toString())
         }
+    }
+
+    private fun createAccount(login: String, pass: String) {
+        auth.createUserWithEmailAndPassword(login, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this,"Usuário criado com sucesso!",LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this,"Ocorreu algum erro na criação. Erro:"+task.exception,LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
